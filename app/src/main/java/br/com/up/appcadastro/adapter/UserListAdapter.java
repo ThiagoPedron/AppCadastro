16103a73b60a3750a7b8bb5234614092c8982ebe
danielezanao <Aluno@DEVE004026.educacional.up>
2022-04-01 20:53:31 -0300

    Initial commit

---
+++ app/src/main/java/br/com/up/appcadastro/adapter/UserListAdapter.java
@@ -0,0 +1,48 @@
+package br.com.up.appcadastro.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import br.com.up.appcadastro.R;
+import br.com.up.appcadastro.model.User;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserViewHolder> {
+    private ArrayList<User> users;
+    public UserListAdapter(ArrayList<User> users){
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater= LayoutInflater.from(parent.getContext());
+
+        View layout = layoutInflater.inflate(R.layout.view_user,parent,false);
+        UserViewHolder userViewHolder = new UserViewHolder(layout);
+        return userViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public  static  class UserViewHolder extends RecyclerView.ViewHolder{
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
